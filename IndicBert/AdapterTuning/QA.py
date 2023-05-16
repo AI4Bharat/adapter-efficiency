32b930576e6f8979febd0f333806c48ea310d9a3
Nandini Mundra <46605294+nandinimundra@users.noreply.github.com>
2023-05-16 09:52:29 +0530

    Update QA.py

--- IndicBert/AdapterTuning/QA.py
+++ IndicBert/AdapterTuning/QA.py
@@ -42,7 +42,7 @@ print(dataset_squad)
 #dataset_squad['train'] = dataset_squad['train'].shard(num_shards=200, index=0)
 #dataset_squad['validation'] = dataset_squad['validation'].shard(num_shards=200, index=0)
 #print(dataset_squad)
-model_name = 'ai4bharat/IndicBERT-MLM-only'
+model_name = 'ai4bharat/IndicBERTv2-MLM-only'
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
 
 max_length = 384
